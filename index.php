aa2e9759ed19610b66a8dd7fe2df4e223afa4c60
Sandhyodip Das <sandhyodip@samarth.ac.in>
2025-08-21 11:24:10 +0530

    Testing..!!

--- index.php
+++ index.php
@@ -1,2 +1,10 @@
 <?php
-echo 'Hello!! My name is Sandhyodip';
+
+require 'vendor/autoload.php';
+use Sandhyodip\MyPackage\Math;
+
+$mathObject = new Math();
+echo $mathObject->sayHello() . "\n";
+
+$additionResult = $mathObject->addition(5, 10);
+echo "Addition Result: " . $additionResult . "\n";
