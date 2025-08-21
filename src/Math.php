3cebb12c5c6a84aaeeea27b65469f7dc7e0556e0
Sandhyodip Das <sandhyodip@samarth.ac.in>
2025-08-21 11:19:33 +0530

    Composer.json file!

---
+++ src/Math.php
@@ -0,0 +1,12 @@
+<?php
+namespace Sandhyodip\MyPackage;
+
+class Math {
+    public function sayHello() {
+        return "Hello from My Package (Math Class)!";
+    }
+
+    public function addition($a, $b) {
+        return $a + $b;
+    }
+}
